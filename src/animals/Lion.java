1ffd0763ede26dbf9f7eb53dcb4fd3a20d914a97
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-07-25 16:00:53 +0500

    Add Lion class with speak method

--- src/animals/Lion.java
+++ src/animals/Lion.java
@@ -1,1 +1,13 @@
+package animals;
 
+public class Lion extends Animal {
+
+    public Lion() {
+        super("Lion");
+    }
+
+    @Override
+    public void speak() {
+        System.out.println(getName() + " says: Roar!");
+    }
+}
