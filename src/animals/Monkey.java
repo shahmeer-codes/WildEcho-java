d8463a69363c6d4306d299f455f5394b60f4e5fb
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-07-25 16:22:22 +0500

    Enhance Monkey class constructor and speak method

--- src/animals/Monkey.java
+++ src/animals/Monkey.java
@@ -1,14 +1,13 @@
-
 package animals;
 
 public class Monkey extends Animal {
 
     public Monkey() {
-        super("Monkey");
+        super("Monkey", "Jungle", "Fruits", 25, "Low");
     }
 
     @Override
     public void speak() {
-        System.out.println(getName() + " says: Oo oo aa aa!");
+        System.out.println("Sound        : Oo oo aa aa!");
     }
 }
