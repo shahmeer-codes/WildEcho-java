5722225bb3c016026f2ce841d61bd65804403b30
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-07-25 16:10:06 +0500

    Add Fox class that extends Animal

--- src/animals/Fox.java
+++ src/animals/Fox.java
@@ -1,1 +1,14 @@
 
+package animals;
+
+public class Fox extends Animal {
+
+    public Fox() {
+        super("Fox");
+    }
+
+    @Override
+    public void speak() {
+        System.out.println(getName() + " says: Ring-ding-ding!");
+    }
+}
