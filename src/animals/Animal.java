f4685c2deeb1266f8c184307006e097960501184
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-07-25 15:58:10 +0500

    Create abstract Animal class with name and speak method

--- src/animals/Animal.java
+++ src/animals/Animal.java
@@ -1,1 +1,17 @@
 
+package animals;
+
+public abstract class Animal {
+
+    protected String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract void speak();
+}
