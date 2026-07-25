27ddbccd1c5019d26b9c0790594da6f561280863
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-07-25 16:02:39 +0500

    Add Tiger class that extends Animal

--- src/animals/Tiger.java
+++ src/animals/Tiger.java
@@ -1,1 +1,14 @@
 
+package animals;
+
+public class Tiger extends Animal {
+
+    public Tiger() {
+        super("Tiger");
+    }
+
+    @Override
+    public void speak() {
+        System.out.println(getName() + " says: Grrrr!");
+    }
+}
