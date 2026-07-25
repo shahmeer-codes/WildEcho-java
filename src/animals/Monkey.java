ee3c63154213fb0b0a60fef5bc8a07966b68b7a3
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-07-25 16:10:40 +0500

    Add Monkey class with speak method

--- src/animals/Monkey.java
+++ src/animals/Monkey.java
@@ -1,1 +1,14 @@
 
+package animals;
+
+public class Monkey extends Animal {
+
+    public Monkey() {
+        super("Monkey");
+    }
+
+    @Override
+    public void speak() {
+        System.out.println(getName() + " says: Oo oo aa aa!");
+    }
+}
