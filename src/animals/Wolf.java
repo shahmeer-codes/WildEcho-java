6acd2727f4b38f708e86d48892af21960d107968
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-07-25 16:07:29 +0500

    Add Wolf class that extends Animal

--- src/animals/Wolf.java
+++ src/animals/Wolf.java
@@ -1,1 +1,13 @@
+package animals;
 
+public class Wolf extends Animal {
+
+    public Wolf() {
+        super("Wolf");
+    }
+
+    @Override
+    public void speak() {
+        System.out.println(getName() + " says: Awoooo!");
+    }
+}
