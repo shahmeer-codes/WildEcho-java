974000696ea1564c7a67aa8c2d831a027c686c84
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-07-25 16:19:50 +0500

    Enhance Tiger class constructor and speak method

--- src/animals/Tiger.java
+++ src/animals/Tiger.java
@@ -1,14 +1,13 @@
-
 package animals;
 
 public class Tiger extends Animal {
 
     public Tiger() {
-        super("Tiger");
+        super("Tiger", "Jungle", "Meat", 20, "High");
     }
 
     @Override
     public void speak() {
-        System.out.println(getName() + " says: Grrrr!");
+        System.out.println("Sound        : Grrrr!");
     }
 }
