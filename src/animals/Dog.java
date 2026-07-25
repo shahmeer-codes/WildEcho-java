a0a37c7d3902f2420fafee8487e9e763e54ffd02
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-07-25 15:59:13 +0500

    Implement Dog class with speak method

--- src/animals/Dog.java
+++ src/animals/Dog.java
@@ -1,1 +1,13 @@
+package animals;
 
+public class Dog extends Animal {
+
+    public Dog() {
+        super("Dog");
+    }
+
+    @Override
+    public void speak() {
+        System.out.println(getName() + " says: Woof! Woof!");
+    }
+}
