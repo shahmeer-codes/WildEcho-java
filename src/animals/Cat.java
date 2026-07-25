82e1c782e0368357a6f98e20919be007f62e0ff7
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-07-25 15:58:43 +0500

    Add Cat class with speak method

--- src/animals/Cat.java
+++ src/animals/Cat.java
@@ -1,1 +1,13 @@
+package animals;
 
+public class Cat extends Animal {
+
+    public Cat() {
+        super("Cat");
+    }
+
+    @Override
+    public void speak() {
+        System.out.println(getName() + " says: Meow!");
+    }
+}
