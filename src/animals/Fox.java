75bf45e83a9a0b9152ff415d2456591e545370d0
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-07-25 16:21:53 +0500

    Update Fox constructor and speak method

--- src/animals/Fox.java
+++ src/animals/Fox.java
@@ -1,14 +1,13 @@
-
 package animals;
 
 public class Fox extends Animal {
 
     public Fox() {
-        super("Fox");
+        super("Fox", "Forest", "Small Animals", 8, "Medium");
     }
 
     @Override
     public void speak() {
-        System.out.println(getName() + " says: Ring-ding-ding!");
+        System.out.println("Sound        : Ring-ding-ding!");
     }
 }
