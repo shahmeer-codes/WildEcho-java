6e96783cbb938b79833c110bac979328f90f4337
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-07-25 16:05:52 +0500

    Add Elephant class with speak method

--- src/animals/Elephant.java
+++ src/animals/Elephant.java
@@ -1,1 +1,13 @@
+package animals;
 
+public class Elephant extends Animal {
+
+    public Elephant() {
+        super("Elephant");
+    }
+
+    @Override
+    public void speak() {
+        System.out.println(getName() + " says: Pawoo!");
+    }
+}
