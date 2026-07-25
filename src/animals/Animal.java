6300b85876f84661ae6bce10b69272f2f1950132
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-07-25 16:15:16 +0500

    Enhance Animal class with additional attributes and methods
    Updated the Animal class to include habitat, food, lifeSpan, and dangerLevel attributes. Modified the constructor and added a showInfo method to display animal details.

--- src/animals/Animal.java
+++ src/animals/Animal.java
@@ -1,12 +1,19 @@
-
 package animals;
 
 public abstract class Animal {
 
     protected String name;
+    protected String habitat;
+    protected String food;
+    protected int lifeSpan;
+    protected String dangerLevel;
 
-    public Animal(String name) {
+    public Animal(String name, String habitat, String food, int lifeSpan, String dangerLevel) {
         this.name = name;
+        this.habitat = habitat;
+        this.food = food;
+        this.lifeSpan = lifeSpan;
+        this.dangerLevel = dangerLevel;
     }
 
     public String getName() {
@@ -14,4 +21,12 @@ public abstract class Animal {
     }
 
     public abstract void speak();
+
+    public void showInfo() {
+        System.out.println("Name         : " + name);
+        System.out.println("Habitat      : " + habitat);
+        System.out.println("Food         : " + food);
+        System.out.println("Life Span    : " + lifeSpan + " years");
+        System.out.println("Danger Level : " + dangerLevel);
+    }
 }
