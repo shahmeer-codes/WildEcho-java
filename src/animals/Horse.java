1a4ad24457fad5386c26eaa3903930a35f46761f
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-07-25 16:07:01 +0500

    Implement Horse class with speak method

--- src/animals/Horse.java
+++ src/animals/Horse.java
@@ -1,1 +1,13 @@
+package animals;
 
+public class Horse extends Animal {
+
+    public Horse() {
+        super("Horse");
+    }
+
+    @Override
+    public void speak() {
+        System.out.println(getName() + " says: Neigh!");
+    }
+}
